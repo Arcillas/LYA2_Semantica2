3e45fded3344dde9e26c2433d6ec89b6f2eb6b3c
Arcillas <l21140773@queretaro.tecnm.mx>
2024-03-27 09:24:29 -0600

    Prueba.cpp

--- prueba.cpp
+++ prueba.cpp
@@ -1,17 +1,53 @@
 #include <stdio.h>
 #include <math.h>
 
-int altura,i,j;
+int radio,altura,i,j;  //Variables de tipo entero
 float x,y,z;
 char c,d,e;
+/*
+    Prueba de fuego 1.0
+*/
 
 void main()
 {
-    x = (30+50)*80-(100-40)/20;
-    printf("\nEl valor de x \t= %f",x);
-    e = (char)x-(13+(char)266);
-    printf("\nIngrese el valor de d \t= ");
-    scanf("%f",&d);
-    printf("\nd = %f",d);
-}
+    if (1!=2)
+    {
+        printf("\nIngrese el valor de d = ");
+        scanf("%f",&d);
+        if (d%2 == 0)
+        {
+            for (i=0; i<d; i++)
+            {
+                printf("-");
+            }
+            printf("\n");
+            for (i=d; i>=0; i--)
+            {
+                printf(" - ",radio);
+            }
+            printf("\n");
+            for (i=0; i<d; i++)
+            {
+                for (j=0; j<=i; j++)
+                {
+                    if (j%2==0)
+                        printf("+");
+                    else
+                        printf("-");
+                }
+                printf("\n");
+            }
+        }
+    }
+	printf("\nVariables:");
+	printf("\n====================");
+	printf("\nradio = %f",radio);
+	printf("\naltura = %f",altura);
+	printf("\nx = %f",x);
+	printf("\ny = %f",y);
+	printf("\nz = %f",z);
+	printf("\nc = %f",c);
+	printf("\nd = %f",d);
+	printf("\ne = %f",e);
 
+}
